56f4721d1e0d3c164da81e643cc735659c80abcb
Censurados <osvaldosouza53@outlook.com>
2022-07-16 23:20:04 -0300

    aula 26

--- aula-026-imc/assets/js/main.js
+++ aula-026-imc/assets/js/main.js
@@ -1,29 +1,68 @@
-function calcularIMC () {
+const form = document.querySelector('#formulario');
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const inputPeso = e.target.querySelector('#input-peso');
+    const inputAltura = e.target.querySelector('#input-altura');
     
-    let peso = document.querySelector('#input-peso');
-    let altura = document.querySelector('#input-altura');
-    const resultado = document.querySelector('#resultado');
+    const peso = Number(inputPeso.value); //se retornar NaN é falsy...
+    const altura = Number(inputAltura.value);
 
-    peso = peso.value;
-    altura = altura.value;
+    if (!peso) {                          //...se peso for falsy:
+        setResultado('Peso inválido', false);
+        return;
+    } 
 
-    let imc = peso / (altura ** 2);
-    let avaliacao;
-
-    if (imc < 18.5) {
-        avaliacao = '(Abaixo do peso)';
-    } else if (imc >= 18.5 && imc < 25) {
-        avaliacao = '(Peso normal)';
-    } else if (imc >= 25 && imc < 30) {
-        avaliacao = '(Sobrepeso)';
-    } else if (imc >= 30 && imc < 35) {
-        avaliacao = '(Obesidade grau 1)';
-    } else if (imc >= 35 && imc < 40) {
-        avaliacao = '(Obesidade grau 2)';
-    } else if (imc >= 40) {
-        avaliacao = '(Obesidade grau 3)';
+    if (!altura) {
+        setResultado('Altura inválida', false);
+        return;
     }
 
-    resultado.innerHTML = `Seu IMC é ${parseInt(imc)} ${avaliacao}`;
+    const imc = getImc(peso, altura);
+    const nivelImc = getNivelImc(imc);
+    
+    const msg = `Seu IMC é ${imc} (${nivelImc}).`;
+
+    setResultado(msg, true);
+});
+
+function getNivelImc (imc) {
+    const nivel = ['Abaixo do peso', 'Peso normal', 'Sobrepeso', 'Obesidade grau 1', 'Obesidade grau 2', 'Obesidade grau 3'];
+
+    if (imc >= 39.9) return nivel[5]; //inverteu a logica p/ checar de tras p/ frente
+    if (imc >= 34.9) return nivel[4];
+    if (imc >= 29.9) return nivel[3];
+    if (imc >= 24.9) return nivel[2];
+    if (imc >= 18.5) return nivel[1];
+    if (imc < 18.5) return nivel[0];
 }
+
+function getImc (peso, altura) {
+    const imc = peso / altura ** 2;
+    return imc.toFixed(2);
+}
+
+function criaP () {
+    const p = document.createElement('p');
+    return p;
+}
+
+// isValid é true ou false? vem lá da validação do peso e altura...
+function setResultado (msg, isValid) { 
+    const resultado = document.querySelector('#resultado');
+    resultado.innerHTML = '';
+
+    const p = criaP();
+
+    if (isValid) {
+        p.classList.add('paragrafo-resultado');
+    } else {
+        p.classList.add('bad');
+    }
+
+    p.innerHTML = msg;
+    resultado.appendChild(p);
+}
+
+
 
