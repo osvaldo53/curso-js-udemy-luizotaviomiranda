6e9e025d8f400255ee9f0a3f77cef96423bc000e
Censurados <osvaldosouza53@outlook.com>
2022-07-24 14:06:12 -0300

    Create 1-for-classico.js

---
+++ aula-034/1-for-classico.js
@@ -0,0 +1,15 @@
+
+for (let i = 0; i <= 5; i++) {
+    const par = i % 2 === 0 ? 'par' : 'impar';
+    console.log(i, par)
+}
+
+
+// // // //
+
+
+const frutas = ['Maçã', 'Pera', 'Uva', 'Banana'];
+
+for (let i = 0; i < frutas.length; i++) {
+    console.log(`Indice ${i}: ${frutas[i]}`);
+}
